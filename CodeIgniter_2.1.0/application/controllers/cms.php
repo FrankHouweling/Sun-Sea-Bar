faf04302e3d44f50654c5800d3188dd9cecdf1db
Frank Houweling <houweling.frank@gmail.com>
2012-04-04 14:43:48 +0200

    Revert "And again some changes."
    This reverts commit 72a574995d49da5610ca5972c8d9093bcbba91d2.

--- CodeIgniter_2.1.0/application/controllers/cms.php
+++ CodeIgniter_2.1.0/application/controllers/cms.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Beheer extends CI_Controller {
+class Cms extends CI_Controller {
 
 	public function index()
 	{
--- CodeIgniter_2.1.0/application/controllers/ticker.php
+++ CodeIgniter_2.1.0/application/controllers/ticker.php
@@ -1,38 +1,11 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Ticker extends CI_Controller 
-{
-
-	/*
-	 * 
-	 * Public function index()
-	 * 
-	 * Will display a page with all the newest menu-items; twitter-messages etc. ordered by newest - last
-	 * 
-	 */
+class Ticker extends CI_Controller {
 
 	public function index()
 	{
-		
-		//	TODO
-		
+		$this->load->view('welcome_message');
 	}
-	
-	/*
-	 * 
-	 * Public function ajaxMessage( $nr = 1 )
-	 * 
-	 * Will give the "$nr"'st menu-item/tweet etc. in HTML for in the homepage-ticker.
-	 * 
-	 */
-	
-	public function ajaxMessage( $nr = 1 )
-	{
-		
-		// TODO
-		
-	}
-	
 }
 
 ?>
