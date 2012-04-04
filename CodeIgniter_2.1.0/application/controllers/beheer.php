2360c944b6bc357d0e35659701a1ae2fb31c0d9d
Frank Houweling <houweling.frank@gmail.com>
2012-04-04 14:44:10 +0200

    Revert "Revert "And again some changes.""
    This reverts commit faf04302e3d44f50654c5800d3188dd9cecdf1db.

--- CodeIgniter_2.1.0/application/controllers/beheer.php
+++ CodeIgniter_2.1.0/application/controllers/beheer.php
@@ -1,11 +1,13 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Beheer extends CI_Controller {
+class Beheer extends CI_Controller 
+{
 
 	public function index()
 	{
 		$this->load->view('welcome_message');
 	}
+	
 }
 
 ?>
