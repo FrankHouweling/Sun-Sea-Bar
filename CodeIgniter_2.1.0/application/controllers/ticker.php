72a574995d49da5610ca5972c8d9093bcbba91d2
Frank Houweling <houweling.frank@gmail.com>
2012-04-04 14:42:29 +0200

    And again some changes.

--- CodeIgniter_2.1.0/application/controllers/ticker.php
+++ CodeIgniter_2.1.0/application/controllers/ticker.php
@@ -1,11 +1,38 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Ticker extends CI_Controller {
+class Ticker extends CI_Controller 
+{
+
+	/*
+	 * 
+	 * Public function index()
+	 * 
+	 * Will display a page with all the newest menu-items; twitter-messages etc. ordered by newest - last
+	 * 
+	 */
 
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		
+		//	TODO
+		
 	}
+	
+	/*
+	 * 
+	 * Public function ajaxMessage( $nr = 1 )
+	 * 
+	 * Will give the "$nr"'st menu-item/tweet etc. in HTML for in the homepage-ticker.
+	 * 
+	 */
+	
+	public function ajaxMessage( $nr = 1 )
+	{
+		
+		// TODO
+		
+	}
+	
 }
 
 ?>
