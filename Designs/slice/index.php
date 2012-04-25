356c07dd2afe6d9fba444f549a7a57c9a2aff5b5
Frank Houweling <houweling.frank@gmail.com>
2012-04-25 11:46:04 +0200

    Some work on the slice.

---
+++ Designs/slice/index.php
@@ -0,0 +1,34 @@
+<!doctype>
+<html>
+	
+	<head>
+		
+		<title>
+			Sun Sea Bar - Home
+		</title>
+		
+		<link rel="stylesheet" media="screen" href="client/css/screen.css" />
+		
+	</head>
+	
+	<body>
+		
+		<div id="ticker">
+			
+			<div id="leftfade">
+				<img src="client/img/leftfade.png" alt="" />
+			</div>
+			
+			<div id="content">
+				
+			</div>
+			
+			<div id="rightfade">
+				
+			</div>
+			
+		</div>
+		
+	</body>
+	
+</html>
