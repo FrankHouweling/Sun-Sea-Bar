2e522f8a26282a634a70444dbda7470bfb3da731
Frank Houweling <houweling.frank@gmail.com>
2012-04-26 11:40:38 +0200

    Some fixes after testing in iPhone sim.

---
+++ Designs/menu.php
@@ -0,0 +1,43 @@
+<!doctype html>
+
+<head>
+	<title>Test Thumbnails for Sunsea</title>
+
+		<!-- Jquery Mobile -->	
+
+		<link rel="stylesheet" href="http://code.jquery.com/mobile/1.1.0-rc.1/jquery.mobile-1.1.0-rc.1.min.css" />
+		<script src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
+		<script src="http://code.jquery.com/mobile/1.1.0-rc.1/jquery.mobile-1.1.0-rc.1.min.js"></script>
+
+		<style type="text/css">
+			body{
+				background-color: #008AB8;
+			}
+		</style>
+</head>
+<body>
+
+<div data-role="content">
+		<div class="content-primary">	
+		<ul data-role="listview" data-theme="b">
+			<li><a href="index.html">
+				<img src="/Users/Maurice/Sites/Sun-Sea-Bar/Designs/images/meter_eten.jpg" width="90px" height"55px" />
+				<h3>Voorgerechten</h3>
+				
+			</a></li>
+			<li><a href="index.html">
+				<img src="/Users/Maurice/Sites/Sun-Sea-Bar/Designs/images/mac_sunsea.jpg" width="90px" height"55px" />
+				<h3>Hoofdgerechten</h3>
+				
+			</a></li>
+			<li><a href="index.html">
+				<img src="/Users/Maurice/Sites/Sun-Sea-Bar/Designs/images/dessert.jpg" width="90px" height"55px" />
+				<h3>Nagerechten</h3>
+				
+			</a></li>
+		</ul>
+		</div>
+</div>
+
+</body>
+</html>
--- Designs/slice/index.php
+++ Designs/slice/index.php
@@ -14,6 +14,10 @@
 		<script src="http://code.jquery.com/jquery-1.6.4.min.js"></script>
 		<script src="http://code.jquery.com/mobile/1.1.0/jquery.mobile-1.1.0.min.js"></script>
 
+		<meta name="apple-mobile-web-app-capable" content="yes" />  
+		<!--<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" /> -->
+		<meta name="viewport" content = "width = device-width, initial-scale = 1, minimum-scale = 1, maximum-scale = 1" />  
+
 		<script type="text/javascript">
 			
 						
@@ -51,7 +55,7 @@
 			</div>
 			
 			<div id="content">
-				<p>Dit is bijv. een twitterbericht die wordt weergegeven...</p>
+				<p>Dit is bijv. een twitterbericht die wordt...</p>
 			</div>
 			
 			<div id="rightfade">
