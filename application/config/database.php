7bfb9d4fd446f39a5ab2d1553293a9148dec6813
Maurice <maurice.stam@student.uva.nl>
2012-05-09 22:04:32 +0200

    does not work, Frank had to change db settings after syncing each time :(?

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'frahou1993';
+$db['default']['password'] = '';
 $db['default']['database'] = 'Sunseabar';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
